13bffbd01dc0691ff90027549b963433592ab161
Alexis <mateosiavig@gmail.com>
2023-03-27 20:52:52 -0500

    Create RolDAO

---
+++ src/main/java/com/system/Carrito_Compras/repository/IRolDao.java
@@ -0,0 +1,7 @@
+package com.system.Carrito_Compras.repository;
+
+import com.system.Carrito_Compras.entity.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IRolDao extends JpaRepository<Rol, Long> {
+}
