84c04ab27fe915074b94e15711317182f46f3b62
Alexis <mateosiavig@gmail.com>
2023-03-27 21:15:01 -0500

    Config Adicionales

--- src/main/java/com/system/Carrito_Compras/CarritoComprasApplication.java
+++ src/main/java/com/system/Carrito_Compras/CarritoComprasApplication.java
@@ -1,13 +1,26 @@
 package com.system.Carrito_Compras;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class CarritoComprasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarritoComprasApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("*").allowedHeaders("*");
+			}
+		};
+	}
 }
